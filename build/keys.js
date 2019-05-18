2bc340aae0a03203e03f50b33d6780ea076898aa
EnriqueSalazarOrdaz <EnriqueSa7@gmail.com>
2019-05-18 18:38:45 -0500

    agregado de appfor

--- build/keys.js
+++ build/keys.js
@@ -3,7 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = {
     database: {
         host:'157.230.223.107',
-        user:'remoteenrique',
+        user:'monty',
         password:'Arik1340!',
         database: 'ng_games_db'
     }
